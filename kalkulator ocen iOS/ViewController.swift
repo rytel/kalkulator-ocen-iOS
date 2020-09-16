e162ee112c7269b811eb7fdccef3d952d6822c34
Rafal Rytel <rflrytel@gmail.com>
2020-09-16 10:53:39 +0200

    Initial Commit

---
+++ kalkulator ocen iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kalkulator ocen iOS
+//
+//  Created by Rafał Rytel on 16/09/2020.
+//  Copyright © 2020 Rafał Rytel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ kalkulator ocen iOSTests/kalkulator_ocen_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  kalkulator_ocen_iOSTests.swift
+//  kalkulator ocen iOSTests
+//
+//  Created by Rafał Rytel on 16/09/2020.
+//  Copyright © 2020 Rafał Rytel. All rights reserved.
+//
+
+import XCTest
+@testable import kalkulator_ocen_iOS
+
+class kalkulator_ocen_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
